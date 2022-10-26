8ce83ce3483f233a7587e975ace40439ef50136f
rodrigosdw <rodrigosdw@gmail.com>
2022-10-26 02:18:03 -0300

    jsIII done

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -5,42 +5,66 @@
 // No cambies los nombres de las funciones.
 
 function imprimirSumaNumeros() {
-    // Imprime la suma de los números del 1 al 10
-    // Pista: usa un acumulador
-    // Tu código:
+  // Imprime la suma de los números del 1 al 10
+  // Pista: usa un acumulador
+  // Tu código:
+  let acumulador = 0;
+  for (let i = 1; i < 11; i++) {
+    acumulador = acumulador + i;
+  }
+  return acumulador;
 }
 
-function encuentraPares(array){
+function encuentraPares(array) {
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  let pares = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      pares.push(array[i]);
+    }
+  }
+  return pares;
 }
 
-function elevaAlCuadrado(array){
+function elevaAlCuadrado(array) {
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  let cuadrados = [];
+
+  for (let i = 0; i < array.length; i++) {
+    cuadrados.push(Math.pow(array[i], 2));
+  }
+  return cuadrados;
 }
 
-function sumaArray(array){
+function sumaArray(array) {
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  let total = 0;
+  for (let i of array) {
+    total += i;
+  }
+  return total;
 }
 
-function numeroDigitos(num){
+function numeroDigitos(num) {
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length;
 }
-  
-  
-  // No modificar nada debajo de esta línea
-  // --------------------------------
-  
-  module.exports = {
-    imprimirSumaNumeros,
-    encuentraPares,
-    elevaAlCuadrado,
-    sumaArray,
-    numeroDigitos
-  };
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  imprimirSumaNumeros,
+  encuentraPares,
+  elevaAlCuadrado,
+  sumaArray,
+  numeroDigitos,
+};
